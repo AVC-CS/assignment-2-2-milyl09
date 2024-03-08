b650efd1f7e7c26be34f81da152059117bcc88ff
milyl09 <157772785+milyl09@users.noreply.github.com>
2024-03-08 02:52:14 +0000

    done

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def main():
-    """
+    
     ##################################################
     # Comlete your code here
-    Use the same variables: celsius fahrenheit 
+    #Use the same variables: celsius fahrenheit 
     ##################################################
-    """
+    celsius = float (input('enter a temperature in Celsius: '))
+    
+    fahrenheit = (celsius * 1.8) + 32
 
-    """
+    print (f'celsius \t {fahrenheit:.2f}')
+
+    
     ########################################
     # Do not delete the return statement
     ########################################
-    """
+    
     return celsius, fahrenheit
 
 
